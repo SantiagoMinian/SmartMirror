234330df42f3c9254050e5cc60e4ed7cfa6f433a
SantiagoMinian <minian.santiago@gmail.com>
2017-09-29 14:25:44 -0300

    Add basic design

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ let win
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({ width: 800, height: 600, backgroundColor: "#000000", frame: false })
+
+    win.setFullScreen(true)
 
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, "public/html/index.html"),
         protocol: 'file:',
         slashes: true
     }))
