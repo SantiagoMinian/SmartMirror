330c4b00ed2d0d662ec803f3b9a9929d32847c6f
SantiagoMinian <minian.santiago@gmail.com>
2017-10-01 16:21:02 -0300

    Change socket now sends data on Return key pressed

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ const { app, BrowserWindow } = require("electron")
 const path = require("path")
 const url = require("url")
 const socket = require("./sockets/socket.js")
+const shortcuts = require("electron-localshortcut")
 
 let win
 
 function createWindow() {
     win = new BrowserWindow({ width: 800, height: 600, backgroundColor: "#000000", frame: false })
+
+    shortcuts.register(win, "Return", () => {
+        socket.send("Lorem Ipsum", (data) => {
+            console.log("DATA: " + data)
+        })
+    })
 
     win.setFullScreen(true)
 
@@ -17,10 +24,6 @@ function createWindow() {
     }))
 
     win.webContents.openDevTools()
-
-    socket.send("Lorem Ipsum", (data) => {
-        console.log("DATA: " + data)
-    })
 
     win.on("closed", () => {
         win = null
