19d7058c2b9981f4214e3065ca72673a0a0c2d72
SantiagoMinian <minian.santiago@gmail.com>
2017-10-01 16:14:48 -0300

    Adapt socket example

---
+++ sockets/socket.js
@@ -0,0 +1,19 @@
+module.exports = {
+    send: function (data, callback) {
+        const net = require("net")
+
+        const HOST = "127.0.0.1"
+        const PORT = 5000
+
+        const socket = new net.Socket()
+
+        socket.connect(PORT, HOST, function () {
+            socket.write(data)
+        })
+
+        socket.on("data", (data) => {
+            callback(data)
+            socket.destroy()
+        })
+    }
+}
