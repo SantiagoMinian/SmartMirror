a21f353ed8429dffce0867db3d3a0cef00f38d62
SantiagoMinian <minian.santiago@gmail.com>
2017-10-17 16:04:21 -0300

    Update UI responses and take photo after countdown

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow } = require("electron")
 const path = require("path")
 const url = require("url")
-const socket = require("./sockets/socket.js")
 const shortcuts = require("electron-localshortcut")
 
 let win
@@ -10,12 +9,6 @@ function createWindow() {
     win = new BrowserWindow({ width: 800, height: 600, backgroundColor: "#000000", frame: false })
 
     shortcuts.register(win, "Return", () => {
-
-        socket.send("Lorem Ipsum", (data) => {
-            win.webContents.send("Message", {
-                msg: data
-            })
-        })
 
         win.loadURL(url.format({
             pathname: path.join(__dirname, "public/html/register.html"),
@@ -32,7 +25,7 @@ function createWindow() {
         slashes: true
     }))
 
-    win.webContents.openDevTools()
+    //win.webContents.openDevTools()
 
     win.on("closed", () => {
         win = null
